3691c25bf946057490b27e2997ae3b067a6e97f6
benfielding74 <billy.fadden@live.com>
2023-05-05 11:57:31 +0100

    feat: adding iam roles

--- ebs/main.tf
+++ ebs/main.tf
@@ -64,7 +64,52 @@ resource "aws_s3_object" "dockerrun_object" {
     Project = "App-portfolio"
   }
 }
-# TODO: Create Instance Profile 
+# Create Instance Profile 
+resource "aws_iam_instance_profile" "ebs_profile" {
+  name = "EC2-EBS-Profile-TF"
+  role = aws_iam_role.ebs_role.name
+  tags = {
+    Project = "App-portfolio"
+  }
+}
+
+resource "aws_iam_role" "ebs_role" {
+  name = "EC2-EBS-Role-TF"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      },
+      {
+        "Action" : [
+          "cloudwatch:PutMetricData",
+          "ec2:DescribeInstanceStatus",
+          "s3:*"
+        ]
+        "resources" : ["*"]
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
+    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  ]
+
+  tags = {
+    Project = "App-portfolio"
+  }
+}
 # TODO: Create eb app
 # TODO: Create eb version
 # TODO: Create eb environment
